606cc045dd73dff1fae53eb3e961c89c612aa2f9
René van Dorst <opensource@vdorst.com>
2023-07-16 16:54:23 +0200

    Add more documentation around Register values

--- embassy-net-adin1110/src/phy.rs
+++ embassy-net-adin1110/src/phy.rs
@@ -4,19 +4,28 @@ use crate::mdio::MdioBus;
 #[repr(u8)]
 /// Clause 22 Registers
 pub enum RegsC22 {
+    /// MII Control Register
     CONTROL = 0x00,
+    /// MII Status Register
     STATUS = 0x01,
+    /// PHY Identifier 1 Register
     PHY_ID1 = 0x02,
+    /// PHY Identifier 2 Register.
     PHY_ID2 = 0x03,
 }
 
+/// Clause 45 Registers
 #[allow(non_snake_case, dead_code)]
 pub mod RegsC45 {
+    /// Device Address: 0x01
     #[allow(non_camel_case_types, clippy::upper_case_acronyms)]
     #[repr(u16)]
     pub enum DA1 {
+        /// PMA/PMD Control 1 Register
         PMA_PMD_CNTRL1 = 0x0000,
+        /// PMA/PMD Status 1 Register
         PMA_PMD_STAT1 = 0x0001,
+        /// MSE Value Register
         MSE_VAL = 0x830B,
     }
 
@@ -26,11 +35,15 @@ pub mod RegsC45 {
         }
     }
 
+    /// Device Address: 0x03
     #[allow(non_camel_case_types, clippy::upper_case_acronyms)]
     #[repr(u16)]
     pub enum DA3 {
+        /// PCS Control 1 Register
         PCS_CNTRL1 = 0x0000,
+        /// PCS Status 1 Register
         PCS_STAT1 = 0x0001,
+        /// PCS Status 2 Register
         PCS_STAT2 = 0x0008,
     }
 
@@ -40,13 +53,33 @@ pub mod RegsC45 {
         }
     }
 
+    /// Device Address: 0x07
+    #[allow(non_camel_case_types, clippy::upper_case_acronyms)]
+    #[repr(u16)]
+    pub enum DA7 {
+        /// Extra Autonegotiation Status Register
+        AN_STATUS_EXTRA = 0x8001,
+    }
+
+    impl DA7 {
+        pub fn into(self) -> (u8, u16) {
+            (0x07, self as u16)
+        }
+    }
+
+    /// Device Address: 0x1E
     #[allow(non_camel_case_types, clippy::upper_case_acronyms)]
     #[repr(u16)]
     pub enum DA1E {
+        /// System Interrupt Status Register
         CRSM_IRQ_STATUS = 0x0010,
+        /// System Interrupt Mask Register
         CRSM_IRQ_MASK = 0x0020,
+        /// Pin Mux Configuration 1 Register
         DIGIO_PINMUX = 0x8c56,
+        /// LED Control Register.
         LED_CNTRL = 0x8C82,
+        /// LED Polarity Register
         LED_POLARITY = 0x8C83,
     }
 
@@ -56,10 +89,13 @@ pub mod RegsC45 {
         }
     }
 
+    /// Device Address: 0x1F
     #[allow(non_camel_case_types, clippy::upper_case_acronyms)]
     #[repr(u16)]
     pub enum DA1F {
+        /// PHY Subsystem Interrupt Status Register
         PHY_SYBSYS_IRQ_STATUS = 0x0011,
+        /// PHY Subsystem Interrupt Mask Register
         PHY_SYBSYS_IRQ_MASK = 0x0021,
     }
 
